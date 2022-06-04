cf2fe339945f50047e496bea6171f62aaca782bb
nichol20 <nick.nogue28@outlook.com>
2022-06-03 23:37:39 -0300

    adding header

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import '../styles/globals.css'
+import '../components/Header/style.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import { Header } from '../components/Header/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className='social_media_app'>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
